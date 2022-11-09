85e3c45c204e407f0b53b007b439c85ca2aa96d5
Abdirahman Abdulle <112982239+Yourfather6498@users.noreply.github.com>
2022-11-09 14:24:42 -0700

    commit

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// JavaScript Code
+// SIMPLE ADDITION IPO CALCULATOR BY MR.V
+
+// Input
+let CSS1G = document.getElementById("CSS1").value;
+let      = prompt("Enter  Structured  Progamming 1 Grade:");
+let      = prompt("Structured Programming 2 Grade:");
+let      = prompt("Enter Client-Side Scripting 2 Grade:");
+let      = prompt("Enter Project A Grade:");
+
+
+//Process
+let total = Client-Side Scripting 1  + Structured  Programming 1 +Structured Programming 2  +Enter Client-Side Scripting 2  +Enter Project A;
+
+//Output
+alert(total);
